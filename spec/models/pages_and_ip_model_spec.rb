93599c020e75b884aac55c27159fddf78e9e594d
golux-System-Production-Name <cveta993.993@gmail.com>
2021-04-18 17:46:00 +0200

    Rspec small model update

--- spec/models/pages_and_ip_model_spec.rb
+++ spec/models/pages_and_ip_model_spec.rb
@@ -1,38 +1,44 @@
 require "rails_helper"
 
 RSpec.describe PagesAndIp, type: :model do
+   #It can be done by just using let and changing one of values when needed
+    # let (:log_entry) do
+    #     PagesAndIp.new(   
+    #         page: ('/home'),
+    #        ip: ('192.169.0.1')
+    #     )
+
     it 'has a valid page and ip' do
-        post = PagesAndIp.new(
+        log_entry = PagesAndIp.new(
             page: ('/home'),
             ip: ('192.169.0.1')
         )
-
-        expect(post).to be_valid
+        expect(log_entry).to be_valid
     end
 
     it 'invalid result due to ip' do
-        post = PagesAndIp.new(
+        log_entry = PagesAndIp.new(
             page: ('/home'),
             ip: ('1.1')
             #ip:('266.266.266.266')
         )
-        expect(post).to_not be_valid
+        expect(log_entry).to_not be_valid
     end
 
     it 'invalid result due to page' do
-        post = PagesAndIp.new(
+        log_entry = PagesAndIp.new(
             page: ('L'),
             ip: ('192.169.0.1')
         )
-        expect(post).to_not be_valid
+        expect(log_entry).to_not be_valid
     end
 
     it 'invalid result due to page and ip' do
-        post = PagesAndIp.new(
+        log_entry = PagesAndIp.new(
             page: ('L'),
             ip: ('1.1')
             #ip:('266.266.266.266')
         )
-        expect(post).to_not be_valid
+        expect(log_entry).to_not be_valid
     end
 end
